4ff2fea846d202f7c7146f1b2e20870e3bc05b73
Sergei Bralgin <bralginsergei1@gmail.com>
2026-03-09 15:37:22 +0400

    Добавляет js

---
+++ source/js/modules/site-nav-mobile.js
@@ -0,0 +1,30 @@
+const navToggle = document.querySelector('.site-nav__toggle');
+const nav = document.querySelector('.site-nav');
+const headerWrapper = document.querySelector('.header__wrapper');
+const overlay = document.querySelector('.overlay');
+
+const openedMenu = () => {
+    nav.classList.add('site-nav--opened');
+    headerWrapper.classList.add('header__wrapper--opened-menu');
+    overlay.classList.add('overlay--active');
+    document.documentElement.style.overflow = 'hidden';
+}
+
+const closedMenu = () => {
+    nav.classList.remove('site-nav--opened');
+    headerWrapper.classList.remove('header__wrapper--opened-menu');
+    overlay.classList.remove('overlay--active');
+    document.documentElement.style.overflow = '';
+}
+
+navToggle.addEventListener('click', () => {
+    if (!nav.classList.contains('site-nav--opened')) {
+        openedMenu();
+    } else {
+        closedMenu();
+    }
+})
+
+overlay.addEventListener('click', () => {
+    closedMenu();
+})
